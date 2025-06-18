509dd5b3bbd53dd596d0a794dd75e84692dc1407
njaswal4 <nisargjaswal83@gmail.com>
2025-06-18 13:54:16 -0400

    updated files

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.2creative.app',
+  appName: 'Productivity_Tool',
+  webDir: 'web/dist',
+
+};
+
+export default config;
